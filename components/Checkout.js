fe2a239884a6d3c7ce6304f06d0152f684c61c39
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-09-19 16:25:10 +0200

    Checkout component

---
+++ components/Checkout.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Checkout.module.css';
+import Spinner from './spinner';
+
+function Checkout() {
+    return (
+        <>
+            <div>La checada</div>
+        </>
+    );
+}
+
+export default Checkout;
